bc3404bccf8e7e4b190c92f242260d17f8d70284
dariusgatsby <145069350+dariusgatsby@users.noreply.github.com>
2023-12-23 18:09:48 -0800

    greyscale function, added jpg upload

--- Camera/camera.py
+++ Camera/camera.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
+
+
+def make_grey(photo):
+    photo = Image.open(photo)
+    gray_scale = photo.convert("L")
+    return gray_scale
+
 
 with st.expander("Camera"):
-    camera_image = st.camera_input("Camera")
+    camera_image = st.camera_input("Smile!")
 
 if camera_image:
-    img = Image.open(camera_image)
-    gray_img = img.convert("L")
-    st.image(gray_img)
+    camera_image = make_grey(camera_image)
+    st.image(camera_image)
+    print(camera_image)
 
+with st.expander("Upload JPEG only"):
+    uploaded_image = st.file_uploader("Upload your file")
+
+if uploaded_image:
+
+    uploaded_image = make_grey(uploaded_image)
+    st.image(uploaded_image)
 
